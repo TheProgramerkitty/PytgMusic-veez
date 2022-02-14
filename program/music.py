c4b763bd18e341976ac9823c9ec1e899a1894f3b
TheProgramerkitty <99088658+TheProgramerkitty@users.noreply.github.com>
2022-02-14 17:04:12 +0530

    Update music.py

--- program/music.py
+++ program/music.py
@@ -212,7 +212,7 @@ async def play(c: Client, m: Message):
                             )
                         else:
                             try:
-                                await suhu.edit("🔄 **Joining vc...**")
+                                await suhu.edit("**PyTgMusic Downloader**\n\n**Title**: {title[:22]}\n\n100% ████████████100%\n\n**Time Taken**: 00:00 Seconds\n\n**Converting Audio[FFmpeg Process]")
                                 await call_py.join_group_call(
                                     chat_id,
                                     AudioPiped(
@@ -242,7 +242,7 @@ async def play(c: Client, m: Message):
                 "» reply to an **audio file** or **give something to search.\n\n Example `/play kanave kanave`**"
             )
         else:
-            suhu = await c.send_message(chat_id, "🔍 **Searching...**")
+            suhu = await c.send_message(chat_id, "**PyTgMusic Downloader**\n\n**Title**: {title[:22]}\n\n100% ████████████100%\n\n**Time Taken**: 00:00 Seconds\n\n**Converting Audio[FFmpeg Process]**")
             query = m.text.split(None, 1)[1]
             search = ytsearch(query)
             if search == 0:
@@ -273,7 +273,7 @@ async def play(c: Client, m: Message):
                         )
                     else:
                         try:
-                            await suhu.edit("🔄 **Joining vc\n PyTgMusic ⚡...**")
+                            await suhu.edit("**PyTgMusic Downloader**\n\n**Title**: {title[:22]}\n\n100% ████████████100%\n\n**Time Taken**: 00:00 Seconds\n\n**Converting Audio[FFmpeg Process]")
                             await call_py.join_group_call(
                                 chat_id,
                                 AudioPiped(
